40f0fb2261d57caccd36157202265bb412007d62
Blade Nelson <powderblock@gmail.com>
2015-07-01 15:45:30 -0400

    Add alt.py
    Create the file alt.py
    In alt.py:
    -import praw
    -set the user_agent

---
+++ alt.py
@@ -0,0 +1,5 @@
+# reddit interface for Python
+import praw
+
+# Set our user_agent so reddit knows what they are dealing with:
+user_agent = ("A reposting script for people who have Imgur blocked at their school / work. Written in PRAW & SBI. See: https://github.com/powderblock/Image-Alt")
