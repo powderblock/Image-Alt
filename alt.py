5e388791903a1a23fbc37237fa2bb5e290054e9e
Blade Nelson <powderblock@gmail.com>
2015-07-01 16:04:29 -0400

    Add oauth_file.py references and files
    In .gitignore:
    -Add oauth_file.py to the ignore list.
    
    In alt.py:
    -Import oauth_file
    -Create out Reddit object
    -Authorize our user_agent using oauth_file.

--- alt.py
+++ alt.py
@@ -1,5 +1,14 @@
 # reddit interface for Python
 import praw
+# oauth_file.py is a local file that contains the account's OAuth info.
+# See the GitHub repo for the example file `oauth_file.py.example`
+import oauth_file
 
 # Set our user_agent so reddit knows what they are dealing with:
 user_agent = ("A reposting script for people who have Imgur blocked at their school / work. Written in PRAW & SBI. See: https://github.com/powderblock/Image-Alt")
+# Setup out Reddit object so that it uses out user_agent
+r = praw.Reddit(user_agent)
+
+# Call the OAuth_Info() function from oauth_file:
+# We pass our file our Reddit object so that it can manipulate it.
+oauth_file.OAuth_Info(r)
